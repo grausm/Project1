72aaac58fcbecbc0be3216b072ed5d8981282d0e
grausm <grausm@student.ncmich.edu>
2017-09-23 23:58:29 -0400

    Initial commit

---
+++ Riverconstruction.js
@@ -0,0 +1,61 @@
+/**
+ *   @author Graus, Michael (grausm@student.ncmich.edu)
+ *   @version 0.0.1
+ *   @summary Project 1 river falls construction || created: 03.16.2016
+ *   @todo
+ */
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+//Section 1 -----------------------------------------^
+
+let lotNumber, bedRooms, bathRooms, garageSize, totalPrice;
+const LANGUAGE = 'English';
+
+//Section 2 -----------------------------------------^
+
+
+function main() {
+    setlotNumber();
+    setbedRooms() ;
+    setbathRooms();
+    setgarageSize();
+    settotalPrice();
+    printResults();
+}
+
+main();
+
+//Section 3 ----------------------------------------^
+
+function setlotNumber() {
+    lotNumber = Number(PROMPT.question(`\nPlease enter lot number: `));
+}
+
+function setbedRooms() {
+    bedRooms = Number(PROMPT.question(`\nHow many bedrooms: `));
+}
+
+function setbathRooms() {
+    bathRooms = Number(PROMPT.question(`\nHow many bathrooms: `));
+}
+
+function setgarageSize() {
+    garageSize = Number(PROMPT.question(`\nHow many garage ports: `));
+}
+
+function settotalPrice() {
+    totalPrice = (bedRooms * 17000) + (bathRooms * 12500)  + (garageSize * 6000);
+}
+
+function printResults() {
+    console.log(`Total Price:   ${totalPrice}`);
+}
+/*
+Design the logic for a program for the River Falls Homes Construction Company.
+Design a program that prompts the user for a lot number in the River Falls subdivision,
+and data about the home to be built there including number of bedrooms, number of bathrooms,
+and size of garage in number of cars it holds. Output is the price of the home which is $50,000 base price plus $17,000 for each bedroom,
+ $12,500 for each bathroom,, and $6,000 for each car the garage holds.
+ */
